77886ce1b64caa820e6b775aa9d19ff8fca0804d
Denilson Nastacio <dnastaci@us.ibm.com>
2022-03-07 11:16:59 -0500

    chore: Auto-promote release at end of testing.

--- tests/postbuild/promote-release.sh
+++ tests/postbuild/promote-release.sh
@@ -29,4 +29,53 @@ cleanRun() {
 trap cleanRun EXIT
 
 
-echo "${release_delta}"
+#
+# Extracts only the file names containing differences between the source
+# and target branches.
+#
+function extract_branch() {
+    result=1
+
+    #
+    # Analyze the differences between branches
+    # to determine which Cloud Paks to test
+    git clone "${git_repo}" cloudpak-gitops -b "${git_target_branch}"
+    cd cloudpak-gitops
+    git config pull.rebase false
+    git checkout "${git_source_branch}"
+    git pull origin "${git_source_branch}"
+
+    return ${result}
+}
+
+
+#
+#
+#
+function merge_and_promote() {
+    release_delta=$1
+
+    latest_version=$(git tag -l --sort=version:refname "v*" | tail -n 1)
+    latest_major_version=$(echo "${latest_version//.*/}" | cut -d "v" -f 2)
+    latest_minor_version=$(echo "${latest_version}" | cut -d "." -f 2)
+    latest_patch_version="${latest_version//v*./}"
+
+    delta_major_version="${release_delta//.*/}"
+    delta_minor_version=$(echo "${release_delta}" | cut -d "." -f 2)
+    delta_patch_version="${release_delta//*./}"
+
+    new_major_version=$((latest_major_version+delta_major_version))
+    new_minor_version=$((latest_minor_version+delta_minor_version))
+    new_patch_version=$((latest_patch_version+delta_patch_version))
+
+    new_version="v${new_major_version}.${new_minor_version}.${new_patch_version}"
+
+    echo "${new_version}"
+
+}
+
+WORKDIR=$(mktemp -d) || exit 1
+cd "${WORKDIR}"
+
+extract_branch
+merge_and_promote "${release_delta}"
